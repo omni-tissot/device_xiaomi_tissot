7ea1129c142a4f7b971f67d9b77aa974415eb0ed
Nick-infnity <nikhilg.me@gmail.com>
2019-07-25 08:44:42 +0000

    Revert "tissot: Add basic USB HAL that reports no status change"
    This reverts commit 72d315d7afcd4312a0e53faf62fdd94a72cce65f.

--- device.mk
+++ device.mk
@@ -105,7 +105,3 @@ $(call inherit-product, build/target/product/verity.mk)
 # Camera
 PRODUCT_PACKAGES += \
     Gcam
-
-#USB HAL
-PRODUCT_PACKAGES += \
-    android.hardware.usb@1.0-service.xiaomi_8996
